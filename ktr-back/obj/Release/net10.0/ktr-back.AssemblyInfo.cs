7e45d5f23703c13970a413eb80058e46f2ece3cb
Abhilash Pattnaik <pixel.abhilash@gmail.com>
2026-09-10 01:00:56 +0530

    Stop tracking .vs and obj folders

--- ktr-back/obj/Release/net10.0/ktr-back.AssemblyInfo.cs
+++ ktr-back/obj/Release/net10.0/ktr-back.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ktr-back")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c35301fbf82c26614e338d0c582b13a7c60f30")]
 [assembly: System.Reflection.AssemblyProductAttribute("ktr-back")]
